a85b4449703cc60f53369fe4db7fecf284bdf904
lwenk <m@lwenk.com>
2026-05-27 17:01:27 +0800

    feat(event): add container output taken source

---
+++ src/mod/event/item/ContainerOutputTakenEvent.cpp
@@ -0,0 +1,73 @@
+﻿#include "mod/event/item/ContainerOutputTakenEvent.h"
+
+#include "ll/api/event/Emitter.h"
+#include "ll/api/event/EventBus.h"
+#include "ll/api/memory/Hook.h"
+
+#include "mc/world/inventory/network/ContainerScreenContext.h"
+#include "mc/world/inventory/network/ItemStackNetManagerServer.h"
+#include "mc/world/inventory/network/ItemStackNetResult.h"
+#include "mc/world/inventory/network/ItemStackRequestActionHandler.h"
+#include "mc/world/inventory/network/ItemStackRequestActionTransferBase.h"
+#include "mc/world/inventory/network/ItemStackRequestSlotInfo.h"
+
+#include <memory>
+
+namespace advancements::event::item {
+namespace {
+
+LL_TYPE_INSTANCE_HOOK(
+    ContainerOutputTakenEventHook,
+    HookPriority::Normal,
+    ItemStackRequestActionHandler,
+    &ItemStackRequestActionHandler::_handleTransfer,
+    ::ItemStackNetResult,
+    ::ItemStackRequestActionTransferBase const& requestAction,
+    bool                                        isSrcHintSlot,
+    bool                                        isDstHintSlot,
+    bool                                        isSwap
+) {
+    auto const& sourceSlot      = *requestAction.mSrc;
+    auto const  sourceContainer = sourceSlot.mFullContainerName.mName;
+    auto const  screenType      = mItemStackNetManager.getScreenContext().mScreenContainerType;
+
+    auto const result = origin(requestAction, isSrcHintSlot, isDstHintSlot, isSwap);
+    if (result != ItemStackNetResult::Success) {
+        return result;
+    }
+
+    ContainerOutputTakenEvent event{mPlayer, sourceContainer, screenType};
+    ll::event::EventBus::getInstance().publish(event);
+    return result;
+}
+
+std::unique_ptr<ll::event::EmitterBase> containerOutputTakenEventEmitterFactory();
+
+class ContainerOutputTakenEventEmitter
+    : public ll::event::Emitter<containerOutputTakenEventEmitterFactory, ContainerOutputTakenEvent> {
+    ll::memory::HookRegistrar<ContainerOutputTakenEventHook> containerOutputTakenHook;
+};
+
+std::unique_ptr<ll::event::EmitterBase> containerOutputTakenEventEmitterFactory() {
+    return std::make_unique<ContainerOutputTakenEventEmitter>();
+}
+
+bool gContainerOutputTakenEventSourceRegistered = false;
+
+} // namespace
+
+bool containerOutputTakenEventSourceRegistered() { return gContainerOutputTakenEventSourceRegistered; }
+
+void registerContainerOutputTakenEventSource() {
+    if (containerOutputTakenEventSourceRegistered()) {
+        return;
+    }
+
+    (void)ContainerOutputTakenEventHook::_AutoHookCount;
+    ll::event::EventBus::getInstance().setEventEmitter<ContainerOutputTakenEvent>(containerOutputTakenEventEmitterFactory);
+    gContainerOutputTakenEventSourceRegistered = true;
+}
+
+void unregisterContainerOutputTakenEventSource() { gContainerOutputTakenEventSourceRegistered = false; }
+
+} // namespace advancements::event::item
---
+++ src/mod/event/item/ContainerOutputTakenEvent.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "ll/api/event/Event.h"
+
+#include "mc/deps/shared_types/legacy/ContainerType.h"
+#include "mc/world/containers/ContainerEnumName.h"
+
+class Player;
+
+namespace advancements::event::item {
+
+class ContainerOutputTakenEvent final : public ll::event::Event {
+public:
+    ContainerOutputTakenEvent(
+        Player&                            player,
+        ContainerEnumName                  sourceContainer,
+        SharedTypes::Legacy::ContainerType screenType
+    )
+    : mPlayer(player),
+      mSourceContainer(sourceContainer),
+      mScreenType(screenType) {}
+
+    [[nodiscard]] Player& player() const { return mPlayer; }
+    [[nodiscard]] ContainerEnumName sourceContainer() const { return mSourceContainer; }
+    [[nodiscard]] SharedTypes::Legacy::ContainerType screenType() const { return mScreenType; }
+
+private:
+    Player&                            mPlayer;
+    ContainerEnumName                  mSourceContainer;
+    SharedTypes::Legacy::ContainerType mScreenType;
+};
+
+bool containerOutputTakenEventSourceRegistered();
+void registerContainerOutputTakenEventSource();
+void unregisterContainerOutputTakenEventSource();
+
+} // namespace advancements::event::item
