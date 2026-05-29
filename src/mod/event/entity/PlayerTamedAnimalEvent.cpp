169dc6dd4ebde80f46e6d1ad933ef2b111c6cf95
lwenk <m@lwenk.com>
2026-05-29 17:16:35 +0800

    feat(event): capture tame animal variants

--- src/mod/event/entity/PlayerTamedAnimalEvent.cpp
+++ src/mod/event/entity/PlayerTamedAnimalEvent.cpp
@@ -10,9 +10,74 @@
 #include "mc/world/actor/player/Player.h"
 
 #include <memory>
+#include <string>
 
 namespace advancements::event::entity {
 namespace {
+
+std::string catVariantId(int variant) {
+    switch (variant) {
+    case 0:
+        return "minecraft:white";
+    case 1:
+        return "minecraft:black";
+    case 2:
+        return "minecraft:red";
+    case 3:
+        return "minecraft:siamese";
+    case 4:
+        return "minecraft:british_shorthair";
+    case 5:
+        return "minecraft:calico";
+    case 6:
+        return "minecraft:persian";
+    case 7:
+        return "minecraft:ragdoll";
+    case 8:
+        return "minecraft:tabby";
+    case 9:
+        return "minecraft:all_black";
+    case 10:
+        return "minecraft:jellie";
+    default:
+        return {};
+    }
+}
+
+std::string wolfVariantId(int variant) {
+    switch (variant) {
+    case 0:
+        return "minecraft:pale";
+    case 1:
+        return "minecraft:ashen";
+    case 2:
+        return "minecraft:black";
+    case 3:
+        return "minecraft:chestnut";
+    case 4:
+        return "minecraft:rusty";
+    case 5:
+        return "minecraft:snowy";
+    case 6:
+        return "minecraft:spotted";
+    case 7:
+        return "minecraft:striped";
+    case 8:
+        return "minecraft:woods";
+    default:
+        return {};
+    }
+}
+
+std::string entityVariantId(std::string const& entityTypeId, int variant) {
+    if (entityTypeId == "minecraft:cat") {
+        return catVariantId(variant);
+    }
+    if (entityTypeId == "minecraft:wolf") {
+        return wolfVariantId(variant);
+    }
+    return {};
+}
 
 LL_TYPE_INSTANCE_HOOK(
     PlayerTamedAnimalEventHook,
@@ -24,12 +89,14 @@ LL_TYPE_INSTANCE_HOOK(
     Player& player
 ) {
     auto const wasTamed = owner.getStatusFlag(ActorFlags::Tamed);
-    auto const entityTypeId = owner.getTypeName();
+    auto const entityTypeId   = owner.getTypeName();
+    auto const variant        = owner.getVariant();
+    auto const variantId      = entityVariantId(entityTypeId, variant);
 
     origin(owner, player);
 
     if (!wasTamed && owner.getStatusFlag(ActorFlags::Tamed)) {
-        PlayerTamedAnimalEvent event{player, entityTypeId};
+        PlayerTamedAnimalEvent event{player, entityTypeId, variantId};
         ll::event::EventBus::getInstance().publish(event);
     }
 }
--- src/mod/event/entity/PlayerTamedAnimalEvent.h
+++ src/mod/event/entity/PlayerTamedAnimalEvent.h
@@ -10,16 +10,19 @@ namespace advancements::event::entity {
 
 class PlayerTamedAnimalEvent final : public ll::event::Event {
 public:
-    PlayerTamedAnimalEvent(Player& player, std::string entityTypeId)
+    PlayerTamedAnimalEvent(Player& player, std::string entityTypeId, std::string entityVariantId)
     : mPlayer(player),
-      mEntityTypeId(std::move(entityTypeId)) {}
+      mEntityTypeId(std::move(entityTypeId)),
+      mEntityVariantId(std::move(entityVariantId)) {}
 
     [[nodiscard]] Player& player() const { return mPlayer; }
     [[nodiscard]] std::string const& entityTypeId() const { return mEntityTypeId; }
+    [[nodiscard]] std::string const& entityVariantId() const { return mEntityVariantId; }
 
 private:
     Player&     mPlayer;
     std::string mEntityTypeId;
+    std::string mEntityVariantId;
 };
 
 bool playerTamedAnimalEventSourceRegistered();
